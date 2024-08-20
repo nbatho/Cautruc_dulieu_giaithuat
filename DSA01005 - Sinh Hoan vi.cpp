fb93e20d37b4b3ff68a9582bb7529a0ba901f056
Pe <nguyenbatho2k5@gmail.com>
2024-08-20 07:58:10 +0700

    Create DSA01005 - Sinh Hoan vi.cpp

---
+++ DSA01005 - Sinh Hoan vi.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int a[15];
+bool used[15] = {false};
+void in() {
+    for (int i = 1;i<=n;i++) {
+        cout << a[i];
+    }
+    cout << " ";
+}
+void BackTrack(int i) {
+    for (int j = 1;j<=n;j++) {
+        if (!used[j]) {
+            a[i] = j;
+            used[j] = true;
+            if (i == n) in();
+            else BackTrack(i+1);
+            used[j] = false;
+        }
+    }
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        BackTrack(1);
+        cout << endl;
+    }
+}
