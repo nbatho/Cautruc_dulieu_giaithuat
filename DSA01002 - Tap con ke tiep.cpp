a7931e6be077cb594afd14c12c7932fd5d460670
Pe <nguyenbatho2k5@gmail.com>
2024-08-23 09:14:02 +0700

    Create DSA01002 - Tap con ke tiep.cpp

---
+++ DSA01002 - Tap con ke tiep.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+void sinh() {
+    int a[k + 1];
+        for (int i = 1; i <= k; i++)
+            cin >> a[i];
+        a[0] = -1;
+        for (int i = k; i >= 0; i--)
+        {
+            if (a[i] != n - k + i)
+            {
+                a[i]++;
+                for (int j = i + 1; j <= k; j++)
+                    a[j] = a[j - 1] + 1;
+                break;
+            }
+        }
+        for (int i = 1; i <= k; i++)
+            cout << a[i] << " ";
+        cout << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+       
+        cin >> n >> k;
+        sinh();
+    }
+}
