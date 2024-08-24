1277ce237c59dde50ac6323db8301dc8db6007c7
Pe <nguyenbatho2k5@gmail.com>
2024-08-24 10:06:40 +0700

    DSA01020 - Xau nhi phan truoc.cpp

--- DSA01020 - Xau nhi phan truoc.cpp
+++ DSA01020 - Xau nhi phan truoc.cpp
@@ -1,24 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int a[1005];
 string x;
+
 void sinh() {
-    for (int i = 0 ;i < x.size();i++) {
+    for (int i = 0; i < x.size(); i++) {
         a[i] = x[i] - '0';
     }
     int i = x.size() - 1;
-    while (i >= 0 && a[i] == 0) {
-        a[i] = 1;
-        --i;
-    }
-    if (i >= 0) {
-        a[i] = 0;
-    }
-    for (int i = 0 ;i < x.size();i++) {
-        cout << a[i];
-    }
+    while (i >= 0 && a[i] == 0) --i;
 
+    if (i < 0) {
+        for (int i = 0; i < x.size(); i++) {
+            cout << 1;
+        }
+    } else {
+        a[i] = 0;
+        for (int j = i + 1; j < x.size(); j++) {
+            a[j] = 1;
+        }
+        for (int j = 0; j < x.size(); j++) {
+            cout << a[j];
+        }
+    }
 }
+
 int main() {
     int t;
     cin >> t;
