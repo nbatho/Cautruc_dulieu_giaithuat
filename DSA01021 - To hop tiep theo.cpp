29b4ea54e93354127de8d8fa915d93f097aac39b
Pe <nguyenbatho2k5@gmail.com>
2024-08-27 14:34:33 +0700

    DSA01021 - To hop tiep theo.cpp

--- DSA01021 - To hop tiep theo.cpp
+++ DSA01021 - To hop tiep theo.cpp
@@ -1,42 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n,k;
-int a[1001];
-bool stop = false;
+int a[1005];
+int b[1005];
+bool stop =false;
+int cnt = 0;
 void sinh() {
+    int cnt =0;
     int i = k;
-    while(i>0 && a[i] == n-k+i) {
+    while (i > 0 && a[i] == n-k+i) {
         --i;
     }
-    if (i ==  0) stop = true;
+    if (i == 0) {
+        cout << k << endl;
+    }
     else {
-        a[i] +=1;
-        for (int j = i+1;j  <=k;j++) {
-            a[j] = a[j-1] +1;
+        a[i]++;
+        for (int j = i+1;j<=k;j++) {
+            a[j] = a[j-1] + 1;
         }
-    }
-}
-void solve() {
-    cin >> n >> k;
-    for (int i = 1 ;i<=k;i++) {
-        a[i] = i;
-    }
-    stop = false;
-    while (!stop) {
-        
-        for (int i = 1 ;i <=k;i++) {
-            cout << a[i];
+        for (int i = 1;i<=k;i++) {
+            for (int j = 1;j<=k;j++) {
+                if (a[i] == b[j]) cnt++;
+            }
         }
-        sinh();
-        cout << " ";
+        if (cnt == k) cnt = 0;
+        cout << k - cnt << endl;
     }
     
+}
+
+void solve() {
+    cin >> n>> k;
+    if (k > n) swap(n,k);
+    for (int i = 1 ;i <= k;i++) {
+        cin >> b[i];
+        a[i] = b[i];
+    }
+    sinh();
 }
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        solve(); 
-        cout << endl;
+        solve();
     }
 }
