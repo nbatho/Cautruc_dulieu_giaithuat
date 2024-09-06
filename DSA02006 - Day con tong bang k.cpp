bf40624dcf0d463a1ad9ab3e0adf7d291369946a
Pe <127192174+nbatho@users.noreply.github.com>
2024-09-06 16:02:12 +0700

    DSA02006 - Day con tong bang k.cpp

---
+++ DSA02006 - Day con tong bang k.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,k;
+int a[100], v[100];
+vector <vector<int>> res;
+
+void Try(int i) {
+    for (int j = 0; j <= 1; j++) {
+        a[i] = j;  
+        if (i == n) {
+            int s=0;
+            for (int l = 1;l<=n;l++) {
+                if (a[l]) s+= v[l];
+            }
+            if (s == k) {
+                vector <int> x;
+                for (int l = 1;l<=n;l++) {
+                    if (a[l]) x.push_back(v[l]);
+                }
+                res.push_back(x);
+            }
+        }
+        else Try(i+1);
+    }
+}
+
+int main() {
+    int tc;
+    cin >> tc;  
+    while (tc--) {
+        res.clear();
+       cin >> n >> k;
+       for (int i = 1;i<=n;i++) cin >> v[i];
+       sort(v+1,v+n+1);
+       Try(1);
+       if (res.size() == 0) cout << -1 << endl;
+       else {
+            sort(res.begin(),res.end());
+            for (int i = 0;i< res.size();i++) {
+                cout << '[';
+                for (int j = 0 ;j<res[i].size() - 1;j++)
+                    cout << res[i][j] << ' ';
+                cout << res[i][res[i].size()-1] << "]";
+                cout << " ";
+            }
+       }
+       cout << endl;
+    }
+    return 0;
+}
