61218b3e621b4cd81ac20d8d41b8a8064b5ec734
Pe <127192174+nbatho@users.noreply.github.com>
2024-09-15 14:05:38 +0700

    DSA02033 - So xa cach.cpp

--- DSA02033 - So xa cach.cpp
+++ DSA02033 - So xa cach.cpp
@@ -1,58 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int n;
 int a[15];
 bool used[15];
-vector <vector<int>> res;
+
 void ktao() {
     cin >> n;
-    for (int i = 1;i<=n;i++) {
-        a[i] = i;
-    }
-    res.clear();
-    memset(used,false,sizeof(used));
+    memset(used, false, sizeof(used));
 }
-bool check(vector <int> &tmp) {
-    bool found = true;
-    for (int i = 0 ;i < tmp.size() -1 ;i++) {
-        if (abs(tmp[i+1] - tmp[i]) == 1) {
-            found = false;
+
+bool check() {
+    for (int i = 1; i < n; i++) {
+        if (abs(a[i+1] - a[i]) == 1) {
+            return false;
         }
     }
-    return found;
+    return true;
 }
+
 void Try(int i) {
-    for (int j = 1;j<=n;j++) {
+    for (int j = 1; j <= n; j++) {
         if (!used[j]) {
             a[i] = j;
             used[j] = true;
             if (i == n) {
-                vector <int> tmp;
-                tmp.clear();
-                for (int l =  1;l<=n;l++) {
-                    tmp.push_back(a[l]);
+                if (check()) {
+                    for (int l = 1; l <= n; l++) {
+                        cout << a[l];
+                    }
+                    cout << endl;
                 }
-                if (check(tmp)) {
-                    res.push_back(tmp);
-                }
+            } else {
+                Try(i + 1);
             }
-            else Try(i+1);
             used[j] = false;
         }
     }
 }
+
 int main() {
     int t;
     cin >> t;
     while (t--) {
         ktao();
         Try(1);
-        sort(res.begin(),res.end());
-        for (int i = 0 ;i <res.size();i++) {
-            for (int j = 0 ; j <res[i].size();j++) {
-                cout << res[i][j];
-            }
-            cout << endl;
-        }
     }
 }
