f362a3293f78f4b6fd506bcea672206813883b2f
Pe <127192174+nbatho@users.noreply.github.com>
2024-09-29 19:41:22 +0700

    DSA03007 - Gia tri nho nhat cua bieu thuc.cpp

---
+++ DSA03007 - Gia tri nho nhat cua bieu thuc.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool cmp(long long a,long long b) {
+    return a > b;
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        long long a[n],b[n];
+        for (int i = 0 ;i < n;i++) {
+            cin >> a[i];
+        }
+        for (int i = 0 ;i < n;i++) {
+            cin >> b[i];
+        }
+        sort(a,a+n);
+        sort(b,b+n,cmp);
+        long long  sum = 0;
+        for (int i = 0 ;i < n;i++) {
+            sum += a[i]*b[i];
+        }
+        cout << sum << endl;
+    }
+}
