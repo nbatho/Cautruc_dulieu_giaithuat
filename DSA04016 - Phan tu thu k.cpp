3931bf301ce36f4fa2c32d6f0cea64f31c8bb0a0
Pe <127192174+nbatho@users.noreply.github.com>
2024-10-04 10:24:22 +0700

    DSA04016 - Phan tu thu k.cpp

---
+++ DSA04016 - Phan tu thu k.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int m,n,k;
+        cin >> m >> n >> k;
+        int a[m+n];
+        for (int i = 0 ;i < m+n;i++) {
+            cin >> a[i];
+        }
+        sort(a,a+n+m);
+        cout << a[k-1] << endl;
+    }
+}
