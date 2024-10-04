8fc491f4f812ed2a84116f26f7de4099b01986bb
Pe <127192174+nbatho@users.noreply.github.com>
2024-10-04 10:20:52 +0700

    DSA02029 - Thap ha noi .cpp

---
+++ DSA02029 - Thap ha noi .cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void tower(int n,char a, char b,char c) {
+    if (n == 1) cout << a << " -> " << c << endl;
+    else {
+        tower(n-1,a,c,b);
+        cout << a << " -> " << c << endl;
+        tower(n-1,b,a,c);
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    tower(n,'A','B','C');
+}
