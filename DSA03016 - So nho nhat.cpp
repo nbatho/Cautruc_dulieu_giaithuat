82c865e92576935dbbbac696bf088c35ba76cf93
Pe <127192174+nbatho@users.noreply.github.com>
2024-10-15 10:16:36 +0700

    DSA03016 - So nho nhat.cpp

--- DSA03016 - So nho nhat.cpp
+++ DSA03016 - So nho nhat.cpp
@@ -1,42 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+    vd 34 7 
+    34 - 9 = 25 d =6    st = [9 ]
+    25 - 9 = 16 d = 5   st = [9 9]
+    16 - 9 = 7  d = 4   st = [9 9 9]
+    7 < 9 =>    d = 3   st = [9 9 9 7]
+    d > 0 => st.pop = 7 , st.push(7-1) => st = [9 9 9 6] , d = 3
+    d = 3 > 1 => st.push(0) => st = [9 9 9 6 0 ] d--
+    d = 2 > 1 => st.push(0) => st = [9 9 9 6 0 0] d--
+    d =1 > 0 => st.push(1) => st [9 9 9 6 0 0 1]
+*/
 int main() {
-    int t;
-    cin >> t;
-    while(t--) {
+    int t;cin >> t;
+    while (t--) {
         int s,d;
         cin >> s >> d;
-        stack <int> st;
-        while(s > 0) {
+        stack<int> st;
+        while (s > 0) {
             d--;
             if (s > 9) {
                 st.push(9);
-                s -= 9;
+                s -=9;
             }
             else {
                 st.push(s);
                 break;
             }
         }
-        if (d < 0) cout << -1;
+        if (d < 0) cout << -1 << endl;
         else {
             if (d > 0) {
                 int so = st.top();
                 st.pop();
                 st.push(so-1);
-                while (d > 1) {
+                while  (d > 1) {
                     st.push(0);
                     d--;
                 }
                 st.push(1);
             }
-            while (st.size() > 0 ) {
+            while (st.size() > 0) {
                 cout << st.top();
                 st.pop();
             }
         }
         cout << endl;
-
     }
     return 0;
 }
