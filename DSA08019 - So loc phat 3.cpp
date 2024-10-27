25282ee685dd25d8990dfb7fa4281c35b26d71c5
Pe <127192174+nbatho@users.noreply.github.com>
2024-10-27 15:12:22 +0700

    DSA08019 - So loc phat 3.cpp

---
+++ DSA08019 - So loc phat 3.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        queue <string> q;
+        stack <string> st;
+        q.push("");
+        while (!q.empty()) {
+            string s = q.front();
+            st.push(s);
+            q.pop();
+            if (s.size() == n) continue;
+
+            q.push(s + "6");
+            q.push(s + "8");
+        }
+        cout << st.size() - 1 << endl;
+        while (!st.empty()) {
+            cout << st.top() << " ";
+            st.pop();
+        }
+        cout << endl;
+    }
+}
