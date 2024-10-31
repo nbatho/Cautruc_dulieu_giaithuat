4a98a7bee4fbcc0b0ccc4ff9cf7d621be14f160e
Pe <127192174+nbatho@users.noreply.github.com>
2024-10-31 15:08:09 +0700

    DSA07041 - Bieu thuc dung dai nhat.cpp

---
+++ DSA07041 - Bieu thuc dung dai nhat.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        stack <char> st;
+        for (int i = 0 ;i < s.size();i++) {
+            if (s[i] == '(') st.push(s[i]);
+            else if (s[i] ==')') {
+                if (!st.empty() && st.top() == '(') st.pop();
+                else st.push(s[i]);
+            }
+        }
+        cout << s.size() - st.size();
+        cout << endl;
+    }
+}
