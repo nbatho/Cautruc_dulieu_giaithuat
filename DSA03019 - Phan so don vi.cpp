4bc8f530ea7df1cad46372e1f79a97d3f282d2ea
Pe <127192174+nbatho@users.noreply.github.com>
2024-10-31 14:33:12 +0700

    DSA03019 - Phan so don vi.cpp

---
+++ DSA03019 - Phan so don vi.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+    quy dong 
+    a/b - c/d
+    tu = a*d - c*b
+    mau = b*d
+*/
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int p,q;
+        cin >> p >> q;
+        long long ans;
+        while(true) {
+            if (q % p == 0) {
+                cout << "1/" << q/p << endl;
+                break;
+            }
+            else {
+                ans = q/p + 1;
+                cout << "1/" << ans << " + ";
+                p = p * ans - q;
+                q = q * ans;
+            }
+        }
+    }
+}
