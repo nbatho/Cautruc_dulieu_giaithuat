dc38345772ece7fad4fdb428ceb03bca5bb0956c
Pe <127192174+nbatho@users.noreply.github.com>
2024-11-12 08:40:07 +0700

    DSA09001 - Chuyen danh sach canh sang danh sach ke.cpp

---
+++ DSA09001 - Chuyen danh sach canh sang danh sach ke.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int v,e;
+        cin >> v >> e;
+        vector <int> a[10005];
+        for (int i = 0 ;i < e;i++) {
+            int x,y;cin >> x >> y;
+            a[x].push_back(y);
+            a[y].push_back(x);
+        }
+        for (int i = 1 ;i <= v;i++) {
+            cout << i << ": ";
+            for (auto x:a[i]) {
+                cout << x << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
