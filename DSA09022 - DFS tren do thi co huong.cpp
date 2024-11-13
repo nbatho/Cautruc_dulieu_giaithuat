d7e452c4abdf943797eba4cffe0fcfc7560e8be2
Pe <127192174+nbatho@users.noreply.github.com>
2024-11-13 07:59:51 +0700

    DSA09022 - DFS tren do thi co huong.cpp

---
+++ DSA09022 - DFS tren do thi co huong.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int v,e,u;
+vector <int> a[1005];
+bool visted[1005];
+void Init() {
+    cin >> v >> e >> u;
+    for (int i = 0 ;i < v;i++) a[i].clear();
+    for (int i = 0 ;i < e;i++) {
+        int x,y;cin >> x >> y;
+        a[x].push_back(y);
+    }
+    memset(visted,false,sizeof(visted));
+}
+void DFS(int u) {
+    queue <int> q;
+    q.push(u);
+    cout << u << " ";
+    visted[u] = true;
+    for (auto i: a[u]) {
+        if (!visted[i]) {
+            q.push(i);
+            DFS(i);
+        }
+    }
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        Init();
+        DFS(u);
+        cout << endl;
+    }
+}
