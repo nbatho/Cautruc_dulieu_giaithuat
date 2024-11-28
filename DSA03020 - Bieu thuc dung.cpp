324b6cc15156a4e4fc9a6185be664c52e01ee721
Pe <127192174+nbatho@users.noreply.github.com>
2024-11-28 14:21:43 +0700

    DSA03020 - Bieu thuc dung.cpp

---
+++ DSA03020 - Bieu thuc dung.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+    string s; cin >> s;
+    int ans = 0;
+    for (int i = 0 ;i < s.size();i++) {
+        
+    }
+}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
